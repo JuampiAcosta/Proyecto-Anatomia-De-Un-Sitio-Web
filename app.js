8fef9bb663c23c4cfae8fe2e6df83059f0a85e70
JuampiAcosta <acostajuan678@gmail.com>
2022-08-23 18:48:49 -0300

    Agregando Estilos
    Se le agrego estilos de textos, asi como tambien de colores en el fondo, y tambien el archivo css llamado normalize. Sirve para no cometer errores con css

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const path = require('path');
 const app=express();
-const publicpath=path.resolve(__dirname,'./public');
-app.use(express.static(publicpath));
+const publicfolderPath=path.resolve(__dirname,'./public');
+app.use(express.static(publicfolderPath));
 app.listen(3000,()=>{console.log('Servidor corriendo en el puerto 3000');})
 app.get('/',(req,res)=>{res.sendFile(path.resolve(__dirname,'./views/home.html'))})
